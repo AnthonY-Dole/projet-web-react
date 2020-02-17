9664bc6a0d6acf39a21d4e830aa2f35db11a40f7
Dole.anthony <anthony.dole@etu.unice.fr>
2020-02-17 18:14:12 +0100

    mon test de commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> wesh mon rhey.
+          Edit <code>src/App.js</code> wesh mon rheyous.
         </p>
         <a
           className="App-link"
